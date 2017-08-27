8f2b54a9423a838811b123c1096fbdb983be3e96
noru416 <th3425xogm@gmail.com>
2017-08-27 21:42:07 +0900

    2

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ $('.navigation-button').on('click', function() {
 	$('.navigation').toggleClass('is-active');
 	
 });
+$('.work-navigation-button-previous').on('click', function() {
+	var oldItem = $('.work-item.is-active');
+	var newItem = oldItem.prev();
+
+	if (oldItem.is($('.work-item:first'))) {
+		alert('first-child')
+	} else {
+		oldItem.removeClass('is-active');
+		newItem.addClass('is-active');
+	}
+});
+$('.work-navigation-button-next').on('click', function() {
+	var oldItem = $('.work-item.is-active');
+	var newItem = oldItem.next();
+
+	if (oldItem.is($('.work-item:last'))) {
+		alert('last-child')
+	} else {
+		oldItem.removeClass('is-active');
+		newItem.addClass('is-active');
+	}
+});
